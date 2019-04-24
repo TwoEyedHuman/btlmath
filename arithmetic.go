24be429e623fc6c68e03a86986bc822d1cf923e7
Brandon Locke <btl33@drexel.edu>
2019-04-24 17:40:53 -0400

    Added SumSlice, Add, and Multiply functions.

--- arithmetic.go
+++ arithmetic.go
@@ -1,4 +1,4 @@
-package btlmath
+package main
 
 func IntAbs (x int) (y int) { //an integer absolute value function
 	if x >0 {
@@ -7,4 +7,28 @@ func IntAbs (x int) (y int) { //an integer absolute value function
 		y = -1*x
 	}
 	return
+}
+
+func SumSlice (x []float32) (float32) {
+    var run_sum float32 = 0
+    for i := 0; i < len(x); i++ {
+        run_sum = run_sum + x[i]
+    }
+    return run_sum
+}
+
+func Add (x []float32, y float32) ([]float32) {
+    ret := x
+    for i := 0; i < len(x); i++ {
+        ret[i] = x[i] + y
+    }
+    return ret
+}
+
+func Multiply (x []float32, y float32) ([]float32) {
+    ret := x
+    for i := 0; i < len(x); i++ {
+        ret[i] = x[i] * y
+    }
+    return ret
 }
